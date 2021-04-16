b3d5e065ce1bf325bfc1a087b1387d145f76780f
andikaputraputu <putu.appkey@gmail.com>
2021-04-17 07:01:36 +0800

    first commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Container, Row, Col} from 'reactstrap';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <Container>
+                <h1>Anjing</h1>
+            </Container>
+        )
+    }
+}
+
+export default Dashboard;
